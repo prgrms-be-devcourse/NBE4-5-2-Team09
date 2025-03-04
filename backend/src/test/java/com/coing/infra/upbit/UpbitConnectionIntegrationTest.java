2b8889af216b51af62483b1e88bc4e810a18e770
2hwayoung <hwa3270@gmail.com>
2025-03-04 12:06:31 +0900

    chore: fix indent with action on save

--- backend/src/test/java/com/coing/infra/upbit/UpbitConnectionIntegrationTest.java
+++ backend/src/test/java/com/coing/infra/upbit/UpbitConnectionIntegrationTest.java
@@ -22,36 +22,37 @@ import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 @SpringBootTest
 public class UpbitConnectionIntegrationTest {
 
-    @Autowired
-    private WebSocketClient webSocketClient;
+	@Autowired
+	private WebSocketClient webSocketClient;
 
-    @Value("${upbit.websocket.uri}")
-    private String upbitWebSocketUri;
+	@Value("${upbit.websocket.uri}")
+	private String upbitWebSocketUri;
 
-    @Test
-    @DisplayName("Upbit Connection 연결 성공")
-    public void sucessUpbitWebSocketConnectionPing() throws Exception {
-        // Given
-        CountDownLatch latch = new CountDownLatch(1);
-        WebSocketHandler testHandler = new AbstractWebSocketHandler() {
-            @Override
-            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-                session.sendMessage(new PingMessage(ByteBuffer.wrap("PING".getBytes(StandardCharsets.UTF_8))));
-            }
-            @Override
-            public void handlePongMessage(WebSocketSession session, PongMessage message) {
-                String payload = new String(message.getPayload().array(), StandardCharsets.UTF_8);
-                if ("PING".equals(payload)) {
-                    latch.countDown();
-                }
-            }
-        };
+	@Test
+	@DisplayName("Upbit Connection 연결 성공")
+	public void successUpbitWebSocketConnectionPing() throws Exception {
+		// Given
+		CountDownLatch latch = new CountDownLatch(1);
+		WebSocketHandler testHandler = new AbstractWebSocketHandler() {
+			@Override
+			public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+				session.sendMessage(new PingMessage(ByteBuffer.wrap("PING".getBytes(StandardCharsets.UTF_8))));
+			}
 
-        // when
-        webSocketClient.execute(testHandler, upbitWebSocketUri);
+			@Override
+			public void handlePongMessage(WebSocketSession session, PongMessage message) {
+				String payload = new String(message.getPayload().array(), StandardCharsets.UTF_8);
+				if ("PING".equals(payload)) {
+					latch.countDown();
+				}
+			}
+		};
 
-        // then: 최대 5초 동안 Pong 응답(PING)이 도착하는지 확인
-        boolean connected = latch.await(5, TimeUnit.SECONDS);
-        assertThat(connected).isTrue();
-    }
+		// when
+		webSocketClient.execute(testHandler, upbitWebSocketUri);
+
+		// then: 최대 5초 동안 Pong 응답(PING)이 도착하는지 확인
+		boolean connected = latch.await(5, TimeUnit.SECONDS);
+		assertThat(connected).isTrue();
+	}
 }
