4d1f66121e6f0435f6c9d3f576491af363e262ea
wkdan <jmy8876@gmail.com>
2025-03-04 12:11:25 +0900

    fix: user bug fix
    - Remove useless values from entities
    - secure option false
    - add bouncycastle dependency (use argon2)
    - add refresh token

--- backend/src/main/java/com/coing/CoingApplication.java
+++ backend/src/main/java/com/coing/CoingApplication.java
@@ -2,7 +2,19 @@ package com.coing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SecurityScheme(
+	name = "bearerAuth",
+	type = SecuritySchemeType.HTTP,
+	scheme = "bearer",
+	bearerFormat = "JWT"
+)
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class CoingApplication {
 
