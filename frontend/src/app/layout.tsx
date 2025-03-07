6c4905ef9ff48ce48b0f5f48aaf4cef378a46942
wkdan <jmy8876@gmail.com>
2025-03-07 12:36:44 +0900

    feat: add email verification and user logout functionality with JWT support

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,27 +1,25 @@
-import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { AuthProvider } from "./context/auth-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Coing",
-  description: "Real-time Coin Dashboard",
+    title: "Coing",
+    description: "Real-time Coin Dashboard",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main className="container mx-auto px-4 py-8">{children}</main>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <AuthProvider>
+            <Header />
+            <main className="container mx-auto px-4 py-8">{children}</main>
+        </AuthProvider>
+        </body>
+        </html>
+    );
 }
