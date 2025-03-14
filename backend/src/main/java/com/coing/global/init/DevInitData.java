f8b8abcaaec04b034455461a062d7a9db6e1d593
min429 <lsm990429@gmail.com>
2025-03-14 21:43:35 +0900

    chore: delete profile 'dev'

--- backend/src/main/java/com/coing/global/init/DevInitData.java
+++ backend/src/main/java/com/coing/global/init/DevInitData.java
@@ -12,11 +12,9 @@ import java.nio.file.StandardOpenOption;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Profile("dev")
 @Slf4j
 @Configuration
 public class DevInitData {
